c7a0e16ecbb983434153addde53cc3c332680100
Ferla04 <fer.velas135@gmail.com>
2021-10-16 14:36:22 -0500

    Conceptos Baicos tarea 1

---
+++ modulo2/01ConceptosBasico.js
@@ -0,0 +1,28 @@
+//---------------------------TAREA 1-----------------------------//
+/*Realizar un algoritmo en Javascript que permita calcular la suma, resta, multiplicación
+y división de dos números, donde num1 = 5 y num2 = 7. Mostrar los resultados por consola. */
+
+//Inicializacion de variables
+let num1;
+let num2;
+
+let suma;
+let resta;
+let multiplicacion;
+let division;
+
+//Declaracion de variables
+num1 = 5;
+num2 = 7;
+
+suma = num1 + num2;
+resta = num1 - num2;
+multiplicacion = num1 * num2;
+division =  num1 / num2; 
+
+//Imprimir por consola
+console.log(num1 + ' + ' + num2 + ' = ' +  suma);
+console.log(num1 + ' - ' + num2 + ' = ' +  resta);
+console.log(num1 + ' x ' + num2 + ' = ' +  multiplicacion);
+console.log(num1 + ' / ' + num2 + ' = ' +  division);
+
