08362b99625b942cad640ecdad0f3840b07c0291
Ferla04 <fer.velas135@gmail.com>
2021-10-20 23:56:21 -0500

    Dos funciones mas creadas calculate() y conversion()

--- Sprints/sprint1/js/conversor.js
+++ Sprints/sprint1/js/conversor.js
@@ -1,14 +1,22 @@
 let firstSelect = document.getElementById('firstCoin');
 let secondSelect = document.getElementById('SecondCoin');
+//others
+let inputNum = document.getElementById('inputNumber');
+let message = document.getElementById('message');
+let answer = document.getElementById('answer');
+//options
+let options2;
+//value
 let first;
 let second;
+
 const COINS = ['Elige tu Moneda','Dolar','Peso Mexicano','Peso Colombiano','Euro','Libra Esterlina'];
 const VALUE_OPTION = ['','dolar','pesoMex','pesoCol','euro','libraEs'];
 
 
 //------------------------ FUNCIONES --------------------//
-function hola(){
-    let options2 = document.querySelectorAll('.slct2');
+function noRepeat(firstSelect,secondSelect){
+    options2 = document.querySelectorAll('.slct2');
     first = firstSelect.value
     second =  secondSelect.value;
 
@@ -24,6 +32,89 @@ function hola(){
     })
 }
 
+function calculate(firstSelect,secondSelect,inputNum,message,answer){
+    let valueNum = inputNum.value;
+    first = firstSelect.value
+    second =  secondSelect.value;
+
+    if(first.length < 1 || second.length < 1 || valueNum.length == ''){
+        return message.innerHTML = 'por favor ingrese todos los datos';
+    }else{
+        message.innerHTML = '';
+    }
+
+    answer.innerHTML = `Resultado: $${conversion(first,second,valueNum)}`;
+}
+
+
+function conversion(first,second,valueNum){
+    let result = 0;
+
+    switch (first) {
+        case 'dolar':
+            switch (second) {
+                case 'pesoMex': result = valueNum * 20.21;
+                    break;
+                case 'pesoCol': result = valueNum * 3768.09;
+                   break;
+                case  'euro': result = valueNum * 0.86;
+                    break;
+                case 'libraEs': result = valueNum * 0.72;
+                    break;
+            }
+        break;
+        case 'pesoMex':
+            switch (second) {
+                case 'dolar': result = valueNum * 0.049;
+                    break;
+                case 'pesoCol': result = valueNum * 186.36;
+                   break;
+                case  'euro': result = valueNum * 0.042;
+                    break;
+                case 'libraEs': result = valueNum * 0.036;
+                    break;
+            }
+        break;
+        case 'pesoCol':
+            switch (second) {
+                case 'dolar': result = valueNum * 0.00027;
+                    break;
+                case 'pesoMex': result = valueNum * 0.0054;
+                   break;
+                case  'euro': result = valueNum * 0.00023;
+                    break;
+                case 'libraEs': result = valueNum * 0.00019;
+                    break;
+            }
+        break;
+        case 'euro':
+            switch (second) {
+                case 'dolar': result = valueNum * 1.17;
+                    break;
+                case 'pesoMex': result = valueNum * 23.57;
+                   break;
+                case 'pesoCol': result = valueNum * 4391.58;
+                    break;
+                case 'libraEs': result = valueNum * 0.84;
+                    break;
+            }
+        break;
+        case 'libraEs':
+            switch (second) {
+                case 'dolar': result = valueNum * 1.38;
+                    break;
+                case 'pesoMex': result = valueNum * 23.57;
+                   break;
+                case 'pesoCol': result = valueNum * 5206.37;
+                    break;
+                case 'euro': result = valueNum * 1.19;
+                    break;
+            }
+        break;
+    }
+
+    return result;
+}
 
 //------------------------ CODIGO -----------------------//
 
